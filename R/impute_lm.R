1eeafd72290240bb573f9102c9f6bd43dcd2637a
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-12-18 14:53:56 +0100

    add example to impute a linear model

--- R/impute_lm.R
+++ R/impute_lm.R
@@ -1,6 +1,11 @@
 #' @rdname impute
 #' @importFrom methods setMethod
 #' @param data The dataset holding both the observed and the missing values
+#' @examples
+#' dataset <- generateData(n.year = 10, n.site = 50, n.run = 1)
+#' dataset$Count[sample(nrow(dataset), 50)] <- NA
+#' model <- lm(Count ~ Year + factor(Period) + factor(Site), data = dataset)
+#' impute(model, dataset)
 setMethod(
   f = "impute",
   signature = signature(model = "lm"),
