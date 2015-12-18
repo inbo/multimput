be57fd34df098ac8ea45639544b8a2f40fa5d300
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-12-18 14:39:34 +0100

    add the rawImputed class

---
+++ R/rawImputation_class.R
@@ -0,0 +1,22 @@
+#' The rawimputed class
+#' Hold a dataset, an imputation model and imputed valus
+#' @section Slots:
+#'   \describe{
+#'    \item{\code{Model}}{The imputation model}
+#'    \item{\code{Data}}{a data.frame with the data}
+#'    \item{\code{Imputation}}{a matrix wih imputed values}
+#'   }
+#' @name rawImputed-class
+#' @rdname rawImputed-class
+#' @exportClass rawImputed
+#' @aliases rawImputed-class
+#' @importFrom methods setClass
+#' @docType class
+setClass(
+  Class = "rawImputed",
+  representation = representation(
+    Model = "lm",
+    Data = "data.frame",
+    Imputation = "matrix"
+  )
+)
