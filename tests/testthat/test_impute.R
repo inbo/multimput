b7cb17cf91ede7659fdc95e5152e084bbae332f9
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-12-19 11:15:01 +0100

    add unit tests for impute

---
+++ tests/testthat/test_impute.R
@@ -0,0 +1,34 @@
+describe("impute", {
+  context("impute")
+  dataset <- generateData(n.year = 10, n.site = 50, n.run = 1)
+  dataset$Count[sample(nrow(dataset), 50)] <- NA
+  n.imp <- 50L
+  it("handles lm", {
+    model <- lm(Count ~ Year + factor(Period) + factor(Site), data = dataset)
+    expect_is(
+      imputed <- impute(model, dataset),
+      "rawImputed"
+    )
+    expect_identical(
+      ncol(imputed@Imputation),
+      19L
+    )
+    expect_identical(
+      nrow(imputed@Imputation),
+      sum(is.na(dataset$Count))
+    )
+
+    expect_is(
+      imputed <- impute(model, dataset, n.imp = n.imp),
+      "rawImputed"
+    )
+    expect_identical(
+      ncol(imputed@Imputation),
+      n.imp
+    )
+    expect_identical(
+      nrow(imputed@Imputation),
+      sum(is.na(dataset$Count))
+    )
+  })
+})
