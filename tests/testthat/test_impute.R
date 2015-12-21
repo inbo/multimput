77f1b497105ee41eb350ad7ef1743d38757e159c
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-12-21 12:55:52 +0100

    test that impute() handles sets without missing data correctly

--- tests/testthat/test_impute.R
+++ tests/testthat/test_impute.R
@@ -31,4 +31,26 @@ describe("impute", {
       sum(is.na(dataset$Count))
     )
   })
+
+  it("handles datasets without missing observations", {
+    n.imp <- 19L
+    dataset <- generateData(n.year = 10, n.site = 50, n.run = 1)
+    expect_identical(
+      sum(is.na(dataset$Count)),
+      0L
+    )
+    model <- lm(Count ~ Year + factor(Period) + factor(Site), data = dataset)
+    expect_is(
+      imputed <- impute(model, dataset, n.imp = n.imp),
+      "rawImputed"
+    )
+    expect_identical(
+      ncol(imputed@Imputation),
+      n.imp
+    )
+    expect_identical(
+      nrow(imputed@Imputation),
+      sum(is.na(dataset$Count))
+    )
+  })
 })
