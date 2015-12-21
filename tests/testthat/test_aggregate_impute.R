bdc017d918bb9c4ace8e8958f46199850b982b85
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-12-21 12:56:17 +0100

    test that aggregate_impute() handles sets without imputations correctly

--- tests/testthat/test_aggregate_impute.R
+++ tests/testthat/test_aggregate_impute.R
@@ -24,4 +24,31 @@ describe("aggregate_impute", {
       ncol(aggr@Imputation)
     )
   })
+
+  it("handles datasets without missing observations", {
+    n.imp <- 19L
+    dataset <- generateData(n.year = 10, n.site = 50, n.run = 1)
+    expect_identical(
+      sum(is.na(dataset$Count)),
+      0L
+    )
+    model <- lm(Count ~ Year + factor(Period) + factor(Site), data = dataset)
+    imputed <- impute(model, dataset, n.imp = n.imp)
+    grouping <- c("Year", "Period")
+    fun <- sum
+    expect_is(
+      aggr <- aggregate_impute(imputed, grouping = grouping, fun = fun),
+      "aggregatedImputed"
+    )
+    apply(
+      aggr@Imputation[, -1],
+      2,
+      function(x){
+        expect_identical(
+          x,
+          aggr@Imputation[, 1]
+        )
+      }
+    )
+  })
 })
